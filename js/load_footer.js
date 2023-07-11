79a23bd6b3e83db8edb4496f25a1fa75d1e9d251
mishaschwartz <4380924+mishaschwartz@users.noreply.github.com>
2023-07-11 14:39:55 -0400

    normalize indentation

--- js/load_footer.js
+++ js/load_footer.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
     fetch("/snippets/footer.html").then(resp => resp.text()).then(footer_html => {
         const footer = document.getElementById("footer");
         const template = document.createElement("template");
--- js/load_menu.js
+++ js/load_menu.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
     fetch("/snippets/menu.html").then(resp => resp.text()).then(menu_html => {
         const menu = document.getElementById("menu");
         const template = document.createElement("template");
