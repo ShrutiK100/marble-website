86d1e966e8b34080fb368f446dd5297f79e4c10d
mishaschwartz <4380924+mishaschwartz@users.noreply.github.com>
2023-07-14 16:52:05 -0400

    move banner and pictures html to snippets

---
+++ js/load_pictures.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("/snippets/pictures.html").then(resp => resp.text()).then(pictures_html => {
+        const pictures = document.getElementById("pictures");
+        const template = document.createElement("template");
+        template.innerHTML = pictures_html;
+        pictures.appendChild(template.content)
+    })
+});
