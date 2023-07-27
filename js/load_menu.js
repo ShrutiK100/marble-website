9c266d6da647d46999c4ed0546e383971fe93288
alexandercyu <alexanderc.yu@utoronto.ca>
2023-07-27 14:33:08 -0400

    Update load_menu.js
    Added function to get DACCS logo from node registry and put it in the menu

--- js/load_menu.js
+++ js/load_menu.js
@@ -1,3 +1,24 @@
+function getNodeRegistryInfo() {
+
+    const githubURL = "https://raw.githubusercontent.com/DACCS-Climate/DACCS-node-registry/current-registry/node_registry.json";
+
+    fetch(githubURL).then(resp => resp.json()).then(resp =>  loadMenuLogo(resp) );
+}
+
+function loadMenuLogo(jsonTree){
+    let iconURL = "";
+    let menuLogo = "";
+    let nodeLinkArray = jsonTree["UofT"].links;
+    let nodeLinkArraySize = nodeLinkArray.length;
+
+    for(let x = 0; x < nodeLinkArraySize; x++){
+        let nodeLink = nodeLinkArray[x];
+        if(nodeLink["rel"] == "icon"){
+            menuLogo = document.getElementById("menu-logo");
+            menuLogo.src = nodeLink["href"];
+        }
+    }
+}
 document.addEventListener("DOMContentLoaded", function () {
     fetch("/snippets/menu.html").then(resp => resp.text()).then(menu_html => {
         const menu = document.getElementById("menu");
@@ -5,4 +26,6 @@ document.addEventListener("DOMContentLoaded", function () {
         template.innerHTML = menu_html;
         menu.appendChild(template.content)
     })
+
+    getNodeRegistryInfo();
 });
