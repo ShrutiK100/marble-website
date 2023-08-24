a20cfe1dd2474a7df7ea10f079a99a52ee2a4bbd
alexandercyu <alexanderc.yu@utoronto.ca>
2023-08-24 16:56:31 -0400

    Added node info to node page
    - Added node name, affiliation, description, date added, status, contact information to the node page
    - Moved the unblurred logo image to beside the node name

--- js/node.js
+++ js/node.js
@@ -11,6 +11,7 @@ const converters = {
         const table_row = document.createElement("tr")
         const table_cell_service = document.createElement("td")
         const table_cell_description = document.createElement("td")
+        const table_cell_documentation = document.createElement("td")
         table.classList.add("table", "table-striped");
         table.appendChild(table_head);
         table.appendChild(table_body);
@@ -33,15 +34,34 @@ const converters = {
                     name = `${service.name} ${link.rel}`;
                 }
                 const link_elem = document.createElement("a");
-                console.log(service.links)
-                Object.entries(link).forEach(([attr, value]) => link_elem.setAttribute(attr, value))
-                link_elem.innerText = name;
-                template.content.appendChild(link_elem);
-                template.content.appendChild(document.createElement("br"))
+                //console.log(service.links)
+                Object.entries(link).forEach(([attr, value]) => {
+                    link_elem.setAttribute(attr, value);
+
+                })
+
+                    link_elem.innerText = name;
+                    template.content.appendChild(link_elem);
+                    template.content.appendChild(document.createElement("br"))
+
+                    table_body.appendChild(table_row);
+                    table_row.appendChild(table_cell_service);
+                    table_row.appendChild(table_cell_description);
+                    table_row.appendChild(table_cell_documentation);
+                    table_cell_description.textContent = description;
+                    table_cell_service.appendChild(link_elem);
+
+//Package into a table in this function
 
 
 
-                table_cell_service.appendChild(link_elem);
+
+
+
+
+
+
+
 
 
 
@@ -71,14 +91,43 @@ document.addEventListener("DOMContentLoaded", function () {
                 const elem = document.getElementById(key);
 
                 if (elem !== null) {
+                    console.log(key)
 
                     switch(key){
+                        // Make case for services table here and append to a div
                         case "affiliation":
-                            let title_affiliation = document.createElement("h2");
+                            let title_affiliation = document.createElement("h3");
                             title_affiliation.textContent = val;
                             elem.append((converters[key] || converters["_default"])(title_affiliation));
 
                         break;
+
+                       case "description":
+                            let node_description_title = document.createElement("h5");
+                            node_description_title.textContent = val;
+                            elem.append((converters[key] || converters["_default"])(node_description_title));
+
+                        break;
+
+                        case "last_updated":
+                            let date_added = document.getElementById("date_added");
+                            date_added.textContent = val;
+                            elem.append((converters[key] || converters["_default"])(val));
+
+                        break;
+
+                        case "contact":
+                            let contact_email = document.getElementById("contact_email");
+                            contact_email.textContent = val;
+                            elem.append((converters[key] || converters["_default"])(val));
+                        break;
+
+                        case "contact":
+                            let node_version = document.getElementById("node_version");
+                            node_version.textContent = val;
+                            elem.append((converters[key] || converters["_default"])(val));
+                        break;
+
                         default:
                             elem.append((converters[key] || converters["_default"])(val));
                     }
@@ -89,7 +138,7 @@ document.addEventListener("DOMContentLoaded", function () {
             })
         }
 
-
+        console.log(node_info);
         node_info.links.forEach(link => {
             if (link.rel === "service") {
                 const elem = document.getElementById("url");
@@ -109,11 +158,15 @@ document.addEventListener("DOMContentLoaded", function () {
                 background_elem.style.backgroundSize = "cover";
                 background_elem.style.width = "100%";
                 background_elem.style.height = "100%";
+                background_elem.classList.add("info-background");
 
+                const title_icon_img = document.getElementById("title_icon_img")
+                title_icon_img.classList.add("img-fluid", "mx-auto", "d-block");
+                title_icon_img.setAttribute("src", link.href);
 
-                const background_icon_img = document.getElementById("node-overlay-img")
-                background_icon_img.setAttribute("src", link.href);
-
+                //Used to position the sharp logo overlayed on the blurred logo.
+                //This effect may be used in the future
+                /*
                 switch(node_name) {
                   case "PAVICS":
                     background_icon_img.classList.add("img-fluid","foreground-pavics-img-position");
@@ -128,9 +181,21 @@ document.addEventListener("DOMContentLoaded", function () {
                   default:
                     background_icon_img.classList.add("img-fluid");
                 }
+                */
 
 
             }
+
+
         })
+
+        const node_name_markup = document.createElement("h2");
+        const node_title = document.getElementById("name");
+        node_name_markup.textContent = node_name;
+        node_title.appendChild(node_name_markup)
+
+
+
+
     });
 })
