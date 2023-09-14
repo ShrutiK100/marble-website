6987b70d32ad23190aa3d9f4cdbf4a5e5222b037
alexandercyu <alexanderc.yu@utoronto.ca>
2023-09-14 13:47:37 -0400

    Update load_menu_logo.js
    Renamed file

--- js/load_menu_logo.js
+++ js/load_menu_logo.js
@@ -20,13 +20,14 @@ function loadMenuLogo(jsonTree){
     }
 }
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("/snippets/menu.html").then(resp => resp.text()).then(menu_html => {
+    getNodeRegistryInfo();
+    /*fetch("/templates/partials/menu.html").then(resp => resp.text()).then(menu_html => {
         const menu = document.getElementById("menu");
         const template = document.createElement("template");
         template.innerHTML = menu_html;
         menu.appendChild(template.content);
-        getNodeRegistryInfo();
-    })
+
+    })*/
 
 
 });
